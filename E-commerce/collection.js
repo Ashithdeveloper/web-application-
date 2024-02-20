2f1f635cfbfee78430da994af77fd748b8e63fdc
Ashithdeveloper <160271719+Ashithdeveloper@users.noreply.github.com>
2024-02-20 17:18:34 +0530

    Add files via upload

---
+++ E-commerce/collection.js
@@ -0,0 +1,19 @@
+var productContainer =document.querySelector("#products")
+var search = document.getElementById("search")
+var productlist = productContainer.querySelectorAll("div")
+
+search.addEventListener("keyup",function(event){
+    var enteredValue = event.target.value.toUpperCase();
+
+    for(count=0;count<productlist.length;count=count+1)
+    {
+       var productname=productlist[count].querySelector("p").textContent.toUpperCase();
+       if(productname.indexOf(enteredValue)<0)
+       {
+        productlist[count].style.display="none"
+       }
+       else{
+        productlist[count].style.display="block"
+       }
+    }
+})
